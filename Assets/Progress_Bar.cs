321ad5afa19d4f50a321e14db73d1228cdab9fe1
faranak <faranak.rzv@gmail.com>
2016-09-21 13:33:28 -0700

    Created a simple progress bar for practice

---
+++ Assets/Progress_Bar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Progress_Bar : MonoBehaviour
+{
+    public Image progressTimeBar;
+    public float totalTimeInCurrentScene;
+    //public float totalTimeInSec;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        progressTimeBar.fillAmount += fillingSpeed_Calculator(totalTimeInCurrentScene) *
+            Time.deltaTime;
+        if(progressTimeBar.fillAmount >1f)
+        {
+            this.enabled = false;
+
+        }
+
+	
+	}
+    public float fillingSpeed_Calculator(float totalTimeInSecs)
+    {
+        float fillingSpeed = 1 / totalTimeInSecs;
+        return fillingSpeed;
+    }
+}
